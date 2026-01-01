0c6caddf4f82a18b76fa18a6e1dfafb9581f8ceb
ahorts <godswayahorts@gmail.com>
2026-01-01 18:32:31 +0000

    Refactor fish config: streamline login and interactive session handling

--- dots/.config/fish/config.fish
+++ dots/.config/fish/config.fish
@@ -1,9 +1,5 @@
-function fish_prompt -d "Write out the prompt"
-    # This shows up as USER@HOST /home/user/ >, with the directory colored
-    # $USER and $hostname are set by fish, so you can just use them
-    # instead of using `whoami` and `hostname`
-    printf '%s@%s %s%s%s > ' $USER $hostname \
-        (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+if status is-login
+    source ~/.config/fish/auto-Hypr.fish
 end
 
 if status is-interactive # Commands to run in interactive sessions can go here
@@ -11,23 +7,21 @@ if status is-interactive # Commands to run in interactive sessions can go here
     # No greeting
     set fish_greeting
 
+    starship init fish | source
+    if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
+        cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
+    end
+
+    fastfetch -c ~/.config/fastfetch/examples/16.jsonc
+
+    alias pamcan pacman
+    alias ls 'eza --icons'
+    alias clear "printf '\033[2J\033[3J\033[1;1H'"
+    alias q 'qs -c ii'
+    alias vim nvim
+    alias pn=pnpm
+
 end
-
-starship init fish | source
-if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
-    cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
-end
-
-fastfetch -c ~/.config/fastfetch/examples/16.jsonc
-
-alias pamcan pacman
-alias ls 'eza --icons'
-alias clear "printf '\033[2J\033[3J\033[1;1H'"
-alias q 'qs -c ii'
-alias vim nvim
-alias pn=pnpm
-
-source ~/.config/fish/auto-Hypr.fish
 
 # function fish_prompt
 #   set_color cyan; echo (pwd)
